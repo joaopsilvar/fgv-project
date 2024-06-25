516b0fe98c6a0bb3b33dcc863ada6873b1d2d5bd
João Paulo <joaopaulosilva39.jp@gmail.com>
2024-06-25 11:44:34 -0300

    first commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiURL: 'http://127.0.0.1:8000'
+    //apiURL:'http://streaming-lb-1240536670.us-east-1.elb.amazonaws.com'
+};
---
+++ service/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+echo "Executing the container"
+
+python manage.py migrate
+
+# python populate.py
+
+gunicorn --bind :80 --workers 3 service.wsgi:application
+
+
+exec "$@"
